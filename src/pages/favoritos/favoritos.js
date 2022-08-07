3971c9bf9114724c3b5a4a1b28e0bf61a88d022e
wwpedro <pedrinhosilva35997@gmail.com>
2022-08-07 14:47:22 -0300

    estilizacao de pagina de favoritos

--- src/pages/favoritos/favoritos.js
+++ src/pages/favoritos/favoritos.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
+import "./favoritos.css";
+
 
 function Favoritos(){
     const [filmes, setFilmes] = useState([])
@@ -20,18 +22,29 @@ function Favoritos(){
 
     return(
         <div>
-            {filmes.length===0 && <span>não tem filmes aqui</span>}
-            <ul>
+            <div class="titulo">
+                <h1>Favoritos</h1>
+            </div>
+            <div class="secao">
+                {filmes.length===0 && <span>não tem filmes aqui</span>}
                 {filmes.map((movie)=>{
-                    return(
-                        <li key={movie.id}>
-                            <h4>{movie.title}</h4>
-                            <Link to={`/sobre/${movie.id}`}>Detalhes</Link>
-                            <button onClick={()=>deletar(movie.id)}>deletar</button>
-                        </li>
-                    )
-                })}
-            </ul>
+                        return(
+                            <div class="celula" key={movie.id}>
+                                <div class="imagem">
+                                <img src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`} />
+                                </div>
+                                <div class="conteudo">
+                                    <h1>{movie.title}</h1>
+                                    <div class="botoes">
+                                        <button><Link to={`/sobre/${movie.id}`}>Detalhes</Link></button>
+                                        <button onClick={()=>deletar(movie.id)}>deletar</button>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })}
+                
+            </div>
         </div>
     );
 }
